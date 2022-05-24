ad3a20edc062d184a338a50202e35913d815b355
Sambhav Jain <sambhavjain233@gmail.com>
2022-05-24 22:25:25 +0530

    qr code integration

--- routes/test-creation/testinfo.js
+++ routes/test-creation/testinfo.js
@@ -4,25 +4,26 @@ const randomstring = require("randomstring");
 const Testinfo = require("../../database/testinfo");
 const qo = require("../../database/qo");
 const qopage = require("./qo");
+const QRCode = require('qrcode');
 
 //middlewares ------------------------------------------------------------------------------------------
 let router = express.Router();
-router.use(bodyparser.urlencoded({extended:true}));
+router.use(bodyparser.urlencoded({ extended: true }));
 router.use(express.static("public"));
-router.use("/qo",qopage);
+router.use("/qo", qopage);
 
 
 //routes---------------------------------------------------------------------------------------------------
-router.get("/info",function(req,res){
+router.get("/info", function (req, res) {
     res.render("test-create/testinfo.ejs");
 });
 
-router.post("/info",function(req,res){
+router.post("/info", function (req, res) {
     const testId = randomstring.generate({
         length: 8,
         capitalization: 'lowercase'
     });
-    
+
     const testName = req.body.test_name;
     const time = req.body.time;
     const marks = req.body.marks;
@@ -37,51 +38,59 @@ router.post("/info",function(req,res){
 
     data.save();
 
-    res.redirect("/createtest/qo/"+testId);
+    res.redirect("/createtest/qo/" + testId);
 
 
 });
 
-router.get("/link",function(req,res){
+router.get("/link", function (req, res) {
     const testId = req.query.Id;
-    const link = "http://localhost:3000/testplatform/testinfo/"+testId;
+    const link = "http://localhost:3000/testplatform/testinfo/" + testId;
 
-    qo.findOne({test_id:testId},function(err1, data_qo){
+    qo.findOne({ test_id: testId }, function (err1, data_qo) {
         const nos = data_qo.qo.length;
-        Testinfo.findOne({test_id:testId},function(err2,data_tinfo){
+        Testinfo.findOne({ test_id: testId }, function (err2, data_tinfo) {
             const markperquestion = data_tinfo.marks;
-            const tmarks = nos*markperquestion;
+            const tmarks = nos * markperquestion;
             console.log(tmarks);
-            Testinfo.findOneAndUpdate({test_id:testId},{total_marks:tmarks},function(err3,update){
+            Testinfo.findOneAndUpdate({ test_id: testId }, { total_marks: tmarks }, function (err3, update) {
             });
         });
     });
-    res.render("test-create/linkpage.ejs",{id:testId , link:link});
+    QRCode.toDataURL(link, function (err, code) {
+        if (err) return console.log("error occurred")
+        // console.log(code);
+        res.render("test-create/linkpage.ejs", { id: testId, link: link , image_code : code});
+    });
 });
 
-router.post("/link",function(req,res){
+router.post("/link", function (req, res) {
     const testId = req.query.Id;
-    const link = "http://localhost:3000/testplatform/testinfo/"+testId;
+    const link = "http://localhost:3000/testplatform/testinfo/" + testId;
 
-    qo.findOne({test_id:testId},function(err1, data_qo){
+    qo.findOne({ test_id: testId }, function (err1, data_qo) {
         const nos = data_qo.qo.length;
-        Testinfo.findOne({test_id:testId},function(err2,data_tinfo){
+        Testinfo.findOne({ test_id: testId }, function (err2, data_tinfo) {
             const markperquestion = data_tinfo.marks;
-            const tmarks = nos*markperquestion;
+            const tmarks = nos * markperquestion;
             console.log(tmarks);
-            Testinfo.findOneAndUpdate({test_id:testId},{total_marks:tmarks},function(err3,update){
+            Testinfo.findOneAndUpdate({ test_id: testId }, { total_marks: tmarks }, function (err3, update) {
             });
         });
     });
-    res.render("test-create/linkpage.ejs",{id:testId , link:link});
+    QRCode.toDataURL(link, function (err, code) {
+        if (err) return console.log("error occurred")
+        // console.log(code);
+        res.render("test-create/linkpage.ejs", { id: testId, link: link , image_code : code});
+    });
 });
 
-router.get("/previewtest",function(req,res){
+router.get("/previewtest", function (req, res) {
 
     const testId = req.query.id;
-    qo.findOne({test_id: testId},function(err,data){
+    qo.findOne({ test_id: testId }, function (err, data) {
         console.log(data.qo);
-        res.render("test-create/previewtest.ejs",{render:data.qo});
+        res.render("test-create/previewtest.ejs", { render: data.qo });
     });
 });
 
